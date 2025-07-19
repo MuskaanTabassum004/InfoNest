3b4a1c41e8b737a958cc3feb732d1c7a97d06b30
Muskaan Tabassum <155378730+MuskaanTabassum004@users.noreply.github.com>
2025-07-20 00:57:03 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/InfoNest/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
