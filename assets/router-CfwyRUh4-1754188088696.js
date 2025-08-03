1475893bdf3ed0691ec9984b7baa1f5b1257f432
MuskaanTabassum004 <160422747004@mjcollege.ac.in>
2025-08-03 07:58:19 +0530

    Updates

--- assets/router-CfwyRUh4-1754188088696.js
+++ assets/router-CfwyRUh4-1754188088696.js
@@ -1,4 +1,4 @@
-import{r as s,R as ue}from"./vendor-C733eHZ9-1754187879476.js";/**
+import{r as s,R as ue}from"./vendor-C733eHZ9-1754188088696.js";/**
  * @remix-run/router v1.23.0
  *
  * Copyright (c) Remix Software Inc.
