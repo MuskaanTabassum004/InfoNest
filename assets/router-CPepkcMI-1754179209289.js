3ccfa6abde876bf22bce7c92abeca8ed97366e80
MuskaanTabassum004 <160422747004@mjcollege.ac.in>
2025-08-03 05:30:21 +0530

    Updates

--- assets/router-CPepkcMI-1754179209289.js
+++ assets/router-CPepkcMI-1754179209289.js
@@ -1,4 +1,4 @@
-import{r as s,R as ue}from"./vendor-C733eHZ9-1754179027940.js";/**
+import{r as s,R as ue}from"./vendor-C733eHZ9-1754179209289.js";/**
  * @remix-run/router v1.23.0
  *
  * Copyright (c) Remix Software Inc.
