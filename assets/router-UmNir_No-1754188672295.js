62d0fbf50d2d1ee440aa55be572f9bc2aff7cce9
MuskaanTabassum004 <160422747004@mjcollege.ac.in>
2025-08-03 08:08:03 +0530

    Updates

--- assets/router-UmNir_No-1754188672295.js
+++ assets/router-UmNir_No-1754188672295.js
@@ -1,4 +1,4 @@
-import{r as s,R as ue}from"./vendor-C733eHZ9-1754188088696.js";/**
+import{r as s,R as ue}from"./vendor-C733eHZ9-1754188672295.js";/**
  * @remix-run/router v1.23.0
  *
  * Copyright (c) Remix Software Inc.
