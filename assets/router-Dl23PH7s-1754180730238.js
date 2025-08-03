2fa22caa73913501c6ecd84b1375ab4a4216b120
MuskaanTabassum004 <160422747004@mjcollege.ac.in>
2025-08-03 05:55:40 +0530

    Updates

--- assets/router-Dl23PH7s-1754180730238.js
+++ assets/router-Dl23PH7s-1754180730238.js
@@ -1,4 +1,4 @@
-import{r as s,R as ue}from"./vendor-C733eHZ9-1754180477490.js";/**
+import{r as s,R as ue}from"./vendor-C733eHZ9-1754180730238.js";/**
  * @remix-run/router v1.23.0
  *
  * Copyright (c) Remix Software Inc.
